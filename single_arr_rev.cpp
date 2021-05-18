90bf7896af4c0f79c848958070482421dc715506
sharmila152 <84021583+sharmila152@users.noreply.github.com>
2021-05-18 19:20:37 +0530

    Create single_arr_rev.cpp

---
+++ single_arr_rev.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void single_arr_rev(int i,int n,int a[])
+{
+   if(i>=((n-1)-i))
+      {
+       return;
+      }
+  swap(a[i],a[n-1]);
+   return single_arr_rev(i+1,((n-1)-i)-1,a);
+ } 
+ int main()
+ {
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++)cin>>a[i];
+   int i=0;
+   cout<<single_arr_rev(i,n,a);
+  } 
+   
