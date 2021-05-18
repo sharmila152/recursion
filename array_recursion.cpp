0ffd03085ad7d494c2126ecb2a5c078e2ba9ac01
sharmila152 <84021583+sharmila152@users.noreply.github.com>
2021-05-18 18:24:21 +0530

    Create array_recursion.cpp

---
+++ array_recursion.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void reverse_array(int L,int R,int a[])
+{
+   int temp;
+   if(L>R)
+      {
+       return;
+      }
+   temp=a[L];
+   a[L]=a[R];
+   a[R]=temp;
+   reurn reverse_array(L+1,R-1,a);
+ } 
+ int main()
+ {
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++)cin>>a[i];
+   int L=0,R=n-1;
+   cout<<reverse_array(L,R,a);
+  } 
+   
+
+
